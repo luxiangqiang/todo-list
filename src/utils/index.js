e956294fb6a529e35dbb8ab0e7f6845e072cdab7
luxiangqiang <luxiangqiang@reta-inc.com>
2022-08-08 08:53:46 +0800

    fix: Fix the bug that tasks are not displayed when re-entering the page.

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,21 +2,21 @@
 // Save Tasks
 export const setTasksListLocalstory  = (tasks) => {
   chrome.storage.sync.set({'tasks': tasks}, function() {
-    console.log('😄 Create Tasks Success～');
+    console.log('😄 Create Tasks Success～', tasks);
   })
 }
 
 // Get Tasks
 export const getTasksListLocalstory  = (self) => {
   chrome.storage.sync.get('tasks', (result) => {
-    self.list = result.tasks;
-    console.log('👌 Get Tasks Success～')
+    self.tasks = result.tasks;
+    console.log('👌 Get Tasks Success～', result.tasks)
   });
 }
 
 // Update Tasks 
 export const updateTasksListLocalstory  = (tasks) => {
   chrome.storage.sync.set({'tasks': tasks}, function() {
-    console.log('🚀 Update Tasks Success～');
+    console.log('🚀 Update Tasks Success～', tasks);
   })
 }
